228664caa687294c121dc9dedc399116a1d8765b
Nayan Couto <pereira.nayan@gmail.com>
2024-04-30 02:54:26 -0300

    Unidade_I_Aula_I

---
+++ Unidade_I/Aula_I/Metodos_de_Amostragem_I.py
@@ -0,0 +1,26 @@
+# Amostragem Probabilística
+# Aleatória Simples
+"""
+> O universo de uma pesquisa são todas aquelas pessoas que possuem características necessárias para responder às
+perguntas.
+> A amostra é definida dentro do universo como 'os indivíduos que responderam à pesquisa'.
+
+Características:
+1) A mesma chance.
+2) População conhecida de N elementos.
+3) Você vai selecionar ao acaso N elementos.
+"""
+
+# Amostragem Sistemática
+"""
+1) O tamanho é grande
+2) População de uma cidade.
+3) Planejar um sistema para a seleção da amostra.
+"""
+
+# Amostragem Estratificada
+"""
+1) O tamanho é grande.
+2) População de uma cidade.
+3) A amostra possuí segmentos.
+"""
