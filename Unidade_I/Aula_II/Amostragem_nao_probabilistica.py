08b675be8bfe461d8d38bc532622d44229f8f5de
Nayan Couto <pereira.nayan@gmail.com>
2024-05-01 00:47:42 -0300

    Unidade_I_Aula_II

---
+++ Unidade_I/Aula_II/Amostragem_nao_probabilistica.py
@@ -0,0 +1,32 @@
+# Amostragem Não Probabilística
+"""
+> O universo deve ser estudado;
+> O universo de uma pesquisa são todas as pessoas que possuem características para responder às perguntas;
+
+Amostragem Não probabilística
+> São muito utilizadas;
+> População não conhecida;
+> Selecionar ao acaso (ou não) n elementos;"""
+
+# Amostragem não probabilística por conveniência
+"""
+> Os participantes são voluntários;
+> Escolhidos por conveniência;
+> Rápida e Barata;
+"""
+
+# Amostragem não probabilística por julgamento
+"""
+> Haverá um julgamento prévio;
+> Ter conhecimento aprofundado sobre a população;
+> O pesquisador já possui características definidas por meio do julgamento do próprio pesquisador;
+> Cautela com as generalizações;
+"""
+
+# Amostragem não probabilística por cotas
+"""
+> Variação da técnica de amostragem por julgamento;
+> Cotas de amostragem;
+> Os elementos são formados conforme a composição de categorias que o pesquisador entende como relevantes para a
+pesquisa;
+"""
